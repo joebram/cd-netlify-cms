21bf6060a7653f5a6c5ee096bc01b17df42a6e62
Joseph Bramall <joe@joebramall.com>
2020-03-14 17:16:24 +0000

    Added stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <h3 class="logo">Joe Bramall</h3>
             </Link>
             {/* Hamburger menu */}
             <div
